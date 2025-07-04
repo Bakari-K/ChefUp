b21894144e8b187808b95194559d06128f7fc541
Bakari-K <thebjkerr@gmail.com>
2025-07-04 14:57:34 -0400

    Updated admin.py, settings.py, and urls.py

--- application/admin.py
+++ application/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from application.models import RecipeImage
+
 
 # Register your models here.
+
+class RecipeImageAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'preview')
+
+    def preview(self, obj):
+        return format_html('<img src="{}" style="height: 100px;" />', obj.image.url)
+
+    preview.short_description = 'Preview'
+
+admin.site.register(RecipeImage, RecipeImageAdmin)
