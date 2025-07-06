64c293ff15de07baf22535d54a45b10888a0a14d
Kaiden Joy <qxandthebeast@gmail.com>
2025-07-06 17:42:09 -0400

    Added view for dashboard

--- application/views.py
+++ application/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def dashboard(request):
+    return render(request, 'application/dashboard.html')
+
+def signup(request):
+    pass
