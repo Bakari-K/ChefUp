7bb97d804cbce3c3d91d14e55b11e5b1c06baf78
Kaiden Joy <qxandthebeast@gmail.com>
2025-07-24 15:09:44 -0400

    Added basic recipe form for now while I work on learning CSS and more about forms

--- application/forms.py
+++ application/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from application.models import Recipe, RecipeImage
 
 
 class SignupForm(UserCreationForm):
@@ -66,4 +67,15 @@ class UserSettingsForm(forms.ModelForm):
                 'class': 'w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-red-400 focus:border-red-400',
                 'placeholder': 'Email Address'
             }),
+        }
+
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = ['title', 'description', 'ingredients', 'instructions', 'created_at', 'author', 'is_public']
+        widgets = {
+            'title': forms.TextInput(attrs={}),
+            'description': forms.Textarea(attrs={}),
+            'ingredients': forms.Textarea(attrs={}),
+            'instructions': forms.Textarea(attrs={}),
         }
